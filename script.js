c080dba5e33273afaef5c6cf2f86c009ca61445d
tkseagull <127999092+tkseagull@users.noreply.github.com>
2023-03-15 20:31:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
-    var url = "https://cors-anywhere.herokuapp.com/https://www.reddit.com/r/quotes/top.json?t=week";
-    $.getJSON(url, function(data) {
-        var posts = data.data.children;
-        var html = "<ul>";
-        for (var i = 0; i < 3; i++) {
-            var post = posts[i].data;
-            html += "<li>" + post.title + "</li>";
-        }
-        html += "</ul>";
-        $("#reddit-posts").html(html);
-    });
-});
+			var url = "https://www.reddit.com/r/showerthoughts/top.json?t=week";
+			$.getJSON(url, function(data) {
+				var posts = data.data.children;
+				var html = "<ul class='post-list'>";
+				for (var i = 0; i < 3; i++) { // Only loop through the first 3 posts
+					var post = posts[i].data;
+					html += "<li class='post'>";
+					html += "<h3>" + post.title + "</h3>";
+					if (post.is_self) {
+						html += "<p>" + post.selftext + "</p>";
+					}
+					html += "</li>";
+				}
+				html += "</ul>";
+				$("#reddit-posts").html(html);
+			});
+		});
 
 
 		$(document).ready(function() {
