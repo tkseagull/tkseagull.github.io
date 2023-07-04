245e1dfd73ee269f247cefa698ab9c6a9dfc70a2
tkseagull <127999092+tkseagull@users.noreply.github.com>
2023-07-03 21:23:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,42 @@
- <script>
-        var excelData = [
-            ["Name", "Age", "Email"],
-            ["John Doe", 25, "johndoe@example.com"],
-            ["Jane Smith", 30, "janesmith@example.com"],
-            // Add more rows as needed
-        ];
+    <script>
+        fetch('conversions.csv')
+            .then(response => response.text())
+            .then(data => {
+                var rows = data.split('\n');
+                var excelData = [];
 
-        function displayTable() {
-            var table = document.getElementById("excelTable");
-            var thead = document.createElement("thead");
-            var headerRow = document.createElement("tr");
-
-            for (var i = 0; i < excelData[0].length; i++) {
-                var th = document.createElement("th");
-                th.appendChild(document.createTextNode(excelData[0][i]));
-                headerRow.appendChild(th);
-            }
-
-            thead.appendChild(headerRow);
-            table.appendChild(thead);
-
-            var tbody = document.createElement("tbody");
-
-            for (var i = 1; i < excelData.length; i++) {
-                var row = document.createElement("tr");
-
-                for (var j = 0; j < excelData[i].length; j++) {
-                    var cell = document.createElement(i === 0 ? "th" : "td");
-                    cell.appendChild(document.createTextNode(excelData[i][j]));
-                    row.appendChild(cell);
+                for (var i = 0; i < rows.length; i++) {
+                    excelData.push(rows[i].split(','));
                 }
 
-                tbody.appendChild(row);
-            }
+                var table = document.getElementById("excelTable");
+                var thead = document.createElement("thead");
+                var headerRow = document.createElement("tr");
 
-            table.appendChild(tbody);
-        }
+                for (var i = 0; i < excelData[0].length; i++) {
+                    var th = document.createElement("th");
+                    th.appendChild(document.createTextNode(excelData[0][i]));
+                    headerRow.appendChild(th);
+                }
 
-        function calculate() {
-            var inputNumber = document.getElementById("inputNumber").value;
-            var resultsDiv = document.getElementById("results");
+                thead.appendChild(headerRow);
+                table.appendChild(thead);
 
-            // Perform calculation or logic with the input number
-            // Replace the following line with your actual calculation or logic
-            var result = inputNumber * 2;
+                var tbody = document.createElement("tbody");
 
-            resultsDiv.textContent = "Result: " + result;
-        }
+                for (var i = 1; i < excelData.length; i++) {
+                    var row = document.createElement("tr");
 
-        displayTable();
+                    for (var j = 0; j < excelData[i].length; j++) {
+                        var cell = document.createElement(i === 0 ? "th" : "td");
+                        cell.appendChild(document.createTextNode(excelData[i][j]));
+                        row.appendChild(cell);
+                    }
+
+                    tbody.appendChild(row);
+                }
+
+                table.appendChild(tbody);
+            })
+            .catch(error => console.log(error));
     </script>
-
