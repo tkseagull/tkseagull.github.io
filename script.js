830035afdf116e49c4d77b3f54e250e3feeacf37
tkseagull <127999092+tkseagull@users.noreply.github.com>
2023-07-03 21:29:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+    <script>
+        fetch('conversions.csv')
+            .then(response => response.text())
+            .then(data => {
+                var rows = data.split('\n');
+                var excelData = [];
 
+                for (var i = 0; i < rows.length; i++) {
+                    excelData.push(rows[i].split(','));
+                }
+
+                var table = document.getElementById("excelTable");
+                var thead = document.createElement("thead");
+                var headerRow = document.createElement("tr");
+
+                for (var i = 0; i < excelData[0].length; i++) {
+                    var th = document.createElement("th");
+                    th.appendChild(document.createTextNode(excelData[0][i]));
+                    headerRow.appendChild(th);
+                }
+
+                thead.appendChild(headerRow);
+                table.appendChild(thead);
+
+                var tbody = document.createElement("tbody");
+
+                for (var i = 1; i < excelData.length; i++) {
+                    var row = document.createElement("tr");
+
+                    for (var j = 0; j < excelData[i].length; j++) {
+                        var cell = document.createElement(i === 0 ? "th" : "td");
+                        cell.appendChild(document.createTextNode(excelData[i][j]));
+                        row.appendChild(cell);
+                    }
+
+                    tbody.appendChild(row);
+                }
+
+                table.appendChild(tbody);
+            })
+            .catch(error => console.log(error));
+    </script>
