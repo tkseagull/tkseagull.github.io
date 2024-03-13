c9a7031b0daeac7252557dfc35c3b82348c4749f
tkseagull <127999092+tkseagull@users.noreply.github.com>
2024-03-13 15:35:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+			var url = "https://www.reddit.com/r/showerthoughts/top.json?t=week";
+			$.getJSON(url, function(data) {
+				var posts = data.data.children;
+				var html = "<ul class='post-list'>";
+				for (var i = 0; i < 3; i++) { // Only loop through the first 3 posts
+					var post = posts[i].data;
+					html += "<li class='post'>";
+					html += "<h3>" + post.title + "</h3>";
+					if (post.is_self) {
+						html += "<p>" + post.selftext + "</p>";
+					}
+					html += "</li>";
+				}
+				html += "</ul>";
+				$("#reddit-posts").html(html);
+			});
+		});
+
+		$(document).ready(function() {
+			var url = "https://www.reddit.com/r/lifeprotips/top.json?t=week";
+			$.getJSON(url, function(data) {
+				var posts = data.data.children;
+				var html = "<ul class='post-list'>";
+				for (var i = 0; i < 1; i++) { // Only loop through the first 1 posts
+					var post = posts[i].data;
+					html += "<li class='post'>";
+					html += "<h3>" + post.title + "</h3>";
+					if (post.is_self) {
+						html += "<p>" + post.selftext + "</p>";
+					}
+					html += "</li>";
+				}
+				html += "</ul>";
+				$("#reddit-posts-life").html(html);
+			});
+		});
+		
+		$(document).ready(function() {
+			var url = "https://www.reddit.com/r/philosophy/top.json?t=week";
+			$.getJSON(url, function(data) {
+				var posts = data.data.children;
+				var html = "<ul class='post-list'>";
+				for (var i = 0; i < 3; i++) { // Only loop through the first 1 posts
+					var post = posts[i].data;
+					html += "<li class='post'>";
+					html += "<h3>" + post.title + "</h3>";
+					if (post.is_self) {
+						html += "<p>" + post.selftext + "</p>";
+					}
+					html += "</li>";
+				}
+				html += "</ul>";
+				$("#reddit-posts-philosophy").html(html);
+			});
+		});
+		
+		$(document).ready(function() {
+			var url = "https://www.reddit.com/r/quotes/top.json?t=week";
+			$.getJSON(url, function(data) {
+				var posts = data.data.children;
+				var html = "<ul class='post-list'>";
+				for (var i = 0; i < 3; i++) { // Only loop through the first 1 posts
+					var post = posts[i].data;
+					html += "<li class='post'>";
+					html += "<h3>" + post.title + "</h3>";
+					if (post.is_self) {
+						html += "<p>" + post.selftext + "</p>";
+					}
+					html += "</li>";
+				}
+				html += "</ul>";
+				$("#reddit-posts-quotes").html(html);
+			});
+		});
